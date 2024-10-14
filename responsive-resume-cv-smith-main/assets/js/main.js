4e00daf7d476920f8f8cbb9b8a0651a5eb25669c
Ruuskanen Vivi <ruuskvivi@gmail.com>
2024-10-14 16:03:39 +0300

    cv basis done

--- responsive-resume-cv-smith-main/assets/js/main.js
+++ responsive-resume-cv-smith-main/assets/js/main.js
@@ -55,7 +55,7 @@ window.addEventListener('scroll', scrollTop)
 /*==================== DARK LIGHT THEME ====================*/ 
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
-const iconTheme = 'bx-sun'
+const iconTheme = 'bxs-sun'
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
@@ -63,13 +63,14 @@ const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bxs-moon' : 'bxs-sun'
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+  themeButton.classList[selectedIcon === 'bxs-moon' ? 'add' : 'remove'](iconTheme)
+  console.log(themeButton.classList)
 }
 
 // Activate / deactivate the theme manually with the button
@@ -77,33 +78,51 @@ themeButton.addEventListener('click', () => {
     // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
+    console.log(themeButton.classList)
     // We save the theme and the current icon that the user chose
+    console.log(getCurrentIcon())
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
 /*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/ 
-
+function scaleCV(){
+    document.body.classList.add('scale-cv')
+}
 
 /*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/ 
-
+function removeScale(){
+    document.body.classList.remove('scale-cv')
+}
 
 /*==================== GENERATE PDF ====================*/ 
 // PDF generated area
+let areaCv = document.getElementById('area-cv')
 
+let resumeButton = document.getElementById('resume-button')
 
 // Html2pdf options
-
+let opt = {
+    margin:       0,
+    filename:     'myResume.pdf',
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas:  { scale: 4 },
+    jsPDF:        { format: 'a4', orientation: 'portrait' }
+  };
 
 // Function to call areaCv and Html2Pdf options 
-
+function generateResume(){
+    html2pdf(areaCv, opt)
+}
 
 // When the button is clicked, it executes the three functions
-
+resumeButton.addEventListener('click', () =>{
     // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-
+    scaleCV()
 
     // 2. The PDF is generated
-
+    generateResume()
 
     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+    setTimeout(removeScale, 5000)
+})
